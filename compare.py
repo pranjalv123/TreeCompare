b5a5a85979f35e77c1ee64fa3900b0a9efd4138f
Pranjal Vachaspati <pranjal@pranj.al>
2015-09-08 14:36:10 -0500

    Add command line interface, sample data

--- compare.py
+++ compare.py
@@ -9,7 +9,7 @@ def compare(reference, test, metric):
 
     l = []
     s = []
-    
+
     for i in tb:
         if i not in rb:
             l.append(i)
@@ -22,19 +22,43 @@ def small_clade_size_metric(r, t, b):
     n = bin(b.leafset_bitmask).count('1')
     return min(n, len(t.taxon_namespace) - n)
 
-def rf_matric(r, t, b):
+def rf_metric(r, t, b):
+    return 1
+
+def no_normalization(r, t):
     return 1
 
 
+def ref_normalization(r, t):
+    return len(r.edges())
+
+def test_normalization(r, t):
+    return len(t.edges())
+
 if __name__ == "__main__":
-    t = dendropy.Tree.get_from_path(sys.argv[1], 'newick')
-    r = dendropy.Tree.get_from_path(sys.argv[2], 'newick')
-    l, s = compare(r, t, small_clade_size_metric)
-    pl.hist(s, bins = 1000)
-    count = 0
-    for i in zip(l, s):
-        if i[1] > 100:
-            print i
-            count += 1
-    print count
-    pl.show()
+    parser = argparse.ArgumentParser(description="Compare trees")
+
+    parser.add_argument('-m', '--metric', dest='metric', default='rf')
+    parser.add_argument('-g', '--show-hist', dest='hist', action='store_true')
+    parser.add_argument('-n', '--normalize', dest='normalize', default='ref')
+    parser.add_argument('reference')
+    parser.add_argument('test')
+
+    args = vars(parser.parse_args())
+
+    tn = dendropy.TaxonNamespace()
+    
+    t = dendropy.Tree.get_from_path(args['test'], 'newick', taxon_namespace = tn)
+    r = dendropy.Tree.get_from_path(args['reference'], 'newick', taxon_namespace = tn)
+
+    l, s = compare(r, t, globals()[args['metric'] + '_metric'])
+    print float(sum(s))/float(globals()[(args['normalize'] + '_normalization')](r, t))
+    if args['hist']:
+        pl.hist(s, bins = 1000)
+        count = 0
+        for i in zip(l, s):
+            if i[1] > 100:
+                print i
+                count += 1
+        print count
+        pl.show()
